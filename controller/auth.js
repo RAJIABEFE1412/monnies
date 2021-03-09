ddf5136db814311ba446718c5c757731cc86beca
Raji Rasheed Abolrinwa <31524566+RAJIABEFE1412@users.noreply.github.com>
2021-03-09 14:51:45 -0500

    Update auth.js

--- controller/auth.js
+++ controller/auth.js
@@ -4,7 +4,42 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 
-const User = require("../schemas/Users").User;
+// const User = require("../schemas/Users").User;
+var mongoose = require('mongoose');
+
+var CouponSchema =  new mongoose.Schema({amount:{type:Number},string:{type:String},})
+
+var User = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  coupon:{
+    CouponSchema
+  },
+  userType:{
+    type: String,
+    default: "Investor"
+
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});var User = mongoose.model('users', UserSchema);
+// module.exports = {User};
 const signup = async (req, res, next) => {
     console.log(req.body);
 
